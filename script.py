0f4579c794eb7ed544526537efa78a1ebf5f71a8
Achilleas <sarraxilleas@gmail.com>
2024-09-30 21:48:50 +0300

    added the case for handling pictures

--- script.py
+++ script.py
@@ -5,4 +5,5 @@ files = os.listdir('/Users/axilleas/Downloads')
 for file in files:
     if file.endswith('.pdf') or  file.endswith('.docx') or file.endswith('.txt'):
         shutil.move(f'/Users/axilleas/Downloads/{file}', '/Users/axilleas/Documents')
-    
+    elif file.endswith('.jpg') or  file.endswith('.png') or file.endswith('.jpeg'):
+        shutil.move(f'/Users/axilleas/Downloads/{file}', '/Users/axilleas/Pictures')
