a3984d6e7c07517c92a27410c9e763cfc6995908
Achilleas <sarraxilleas@gmail.com>
2024-09-30 22:34:42 +0300

    debugged the problem wthat occured with files and file paths when removing them

--- script.py
+++ script.py
@@ -3,15 +3,20 @@ import shutil
 
 files = os.listdir('/Users/axilleas/Downloads')
 for file in files:
-    if file.endswith('.pdf') or  file.endswith('.docx') or file.endswith('.txt') or file.endswith('.xls') or file.endswith('.pptx'):
-        shutil.move(f'/Users/axilleas/Downloads/{file}', '/Users/axilleas/Documents')
-    elif file.endswith('.jpg') or  file.endswith('.png') or file.endswith('.jpeg'):
-        shutil.move(f'/Users/axilleas/Downloads/{file}', '/Users/axilleas/Pictures')
+    file_path = f'/Users/axilleas/Downloads/{file}'
+    if file.endswith('.pdf') or file.endswith('.docx') or file.endswith('.txt') or file.endswith('.xls') or file.endswith('.pptx'):
+        shutil.move(file_path, '/Users/axilleas/Documents')
+    elif file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg'):
+        shutil.move(file_path, '/Users/axilleas/Pictures')
     elif file.endswith('.zip') or file.endswith('.tar') or file.endswith('.gz'):
-        shutil.unpack_archive(f'/Users/axilleas/Downloads/{file}', f'/Users/axilleas/Downloads/{file}:unzipped')
-        shutil.rmtree(f'/Users/axilleas/Downloads/{file}')
-        shutil.move(f'/Users/axilleas/Downloads/{file}:unzipped', '/Users/axilleas/Documents')
-    elif file.endswith('.dmg') or file.endswith('.exe'):
-        shutil.rmtree(f'/Users/axilleas/Downloads/{file}')
+        extract_to = f'/Users/axilleas/Downloads/{file}_unzipped'
+        shutil.unpack_archive(file_path, extract_to)
+        os.remove(file_path)
+        shutil.move(extract_to, '/Users/axilleas/Documents')
+    elif file.endswith('.dmg') or file.endswith('.exe') or file.endswith('.DS_Store'):
+        if os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+        else:
+            os.remove(file_path)
     else:
-        shutil.move(f'/Users/axilleas/Downloads/{file}', '/Users/axilleas/Documents')
+        shutil.move(file_path, '/Users/axilleas/Documents')
